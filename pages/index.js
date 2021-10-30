082fdcf164c5bf4cec75ce9dc9a4be45d30d1393
Breno Zukowski <brenozukotech@gmail.com>
2021-10-29 22:03:17 -0300

    Added staging vercel test

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Staging Vercel!</a>
+          Welcome to <a href="https://nextjs.org">Staging Vercel test!</a>
         </h1>
 
         <p className={styles.description}>
